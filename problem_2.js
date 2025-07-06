fc46f5a6cf1da8628a628c21035d828e78cc1d55
baizta404 <chowdhuryhridoy261@gmail.com>
2025-07-07 01:26:37 +0600

    problem 2 solved

---
+++ problem_2.js
@@ -0,0 +1,11 @@
+const oddEven=(num)=> num%2===0?console.log("Even Number"):console.log("Odd Number");
+
+
+// vai programiz a dekhlam prompt diya input neya jay
+//const number = parseInt(prompt("Enter A Number: ")) ;
+//kintu vs code a pari nai.
+
+const number = 11;
+oddEven(number);
+
+
